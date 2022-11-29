70f53db5a90502927643ac2ae67953e606a77941
av84002499 <av84002499@gmail.com>
2022-11-29 12:35:59 +0530

    vidoe

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactPlayer from 'react-player'
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+     <ReactPlayer controls url='https://www.youtube.com/watch?v=AvjvZ7q2apE' />
+    </div>
+    );
+}
+export default App;
